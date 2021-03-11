89940d532a9fa067684206ddfe62d0af957aebec
Michael <michael.weydert@sfr.fr>
2021-03-11 12:13:28 +0100

    Remarques composant Books.vue

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -5,12 +5,12 @@
       <input type="text" v-model="search" placeholder="edit me" />
     </div>
     <div class="books-container" v-if="books.length !== 0">
-      <div v-for="book of filterBook()" :key="book.isbn">
+      <div v-for="book of filterBook" :key="book.isbn">
         <app-book :book="book"></app-book>
       </div>
     </div>
     <div v-if="search !== ''">
-      <div v-if="filterBook().length === 0">no result</div>
+      <div v-if="filterBook.length === 0">no result</div>
     </div>
   </div>
 </template>
@@ -21,22 +21,28 @@ import { Component, Vue } from "vue-property-decorator";
 import { State, Action, Mutation } from "vuex-class";
 import BookDetail from "./BookDetail.vue";
 
+
 @Component({
   components: {
     "app-book": BookDetail,
   },
 })
 export default class Books extends Vue {
-  @Action public getBooks!: () => void;
-  @Mutation public setBooks!: (books: Book[]) => void;
-  @State((state) => state.shopConfig.books) public books!: Book[];
-  public search = "";
+  // Tu n'as pas besoin d'accéder à tes propriétés/méthodes en dehors de ton composant --> pas besoin de les mettre en public
 
-  public mounted() {
+  @Action private getBooks!: () => void;
+  // @Mutation public setBooks!: (books: Book[]) => void; // Plus utilisé 
+  @State((state) => state.shopConfig.books) public books!: Book[];
+  private search = "";
+
+  // Mieux vaut changer en created. Si tu laisse en mounted, tu risque d'avoir ton HTML rendu 2 fois : 
+  // - Une première fois avec this.books qui est une liste vide
+  // - Une 2e fois une fois que tu auras récupéré tes données
+  private created() {
     this.getBooks();
   }
 
-  public filterBook() {
+  private get filterBook() {
     if (this.search !== "") {
       return this.books.filter((el) => el.title.includes(this.search));
     }
